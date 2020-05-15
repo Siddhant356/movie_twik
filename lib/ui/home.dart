47f3c1bc32b66b851b6cf80340a76861dc824c74
Siddhant356 <siddhantshukla356@gmail.com>
2020-05-16 01:00:55 +0530

    movie detail page update

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -33,47 +33,16 @@ class Home extends StatelessWidget {
         title: Text("Movies"),
         backgroundColor: Colors.blueGrey.shade900,
       ),
-      backgroundColor: Colors.blueGrey.shade400,
+      backgroundColor: Colors.blueGrey.shade900,
       body: ListView.builder(
           itemCount: movieList.length,
           itemBuilder: (BuildContext context, int index) {
             return Stack(children: <Widget>[
               movieCard(movieList[index], context),
               Positioned(
-                top: 10.0,
-                  child: movieImage(movieList[index].images[0])),
-              
+                  top: 10.0, child: movieImage(movieList[index].images[0])),
             ]);
-//            return Card(
-//              elevation: 4.5,
-//              color: Colors.white,
-//              child: ListTile(
-//                leading: CircleAvatar(
-//                  child: Container(
-//                    decoration: BoxDecoration(
-//                      image: DecorationImage(
-//                          image: NetworkImage(movieList[index].images[0]),
-//                          fit: BoxFit.cover,
-//                      ),
-//                      //color: Colors.blue,
-//                      borderRadius: BorderRadius.circular(21.9),
-//                    ),
-//                  ),
-//                ),
-//                title: Text(movieList[index].title),
-//                subtitle: Text("${movieList[index].genre}"),
-//                trailing: Text("..."),
-//                // onTap: ()=> debugPrint("${movies.elementAt(index)}"),
-//                onTap: () {
-//                  Navigator.push(
-//                      context,
-//                      MaterialPageRoute(
-//                          builder: (context) => MovieListViewDetails(
-//                              movieName: movieList[index].title,
-//                              movie: movieList[index])));
-//                },
-//              ),
-//            );
+//
           }),
     );
   }
@@ -87,54 +56,78 @@ class Home extends StatelessWidget {
         child: Card(
           color: Colors.black45,
           child: Padding(
-            padding: const EdgeInsets.only(top: 8.0,bottom: 8.0, left: 54.0 ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text("${movie.title}"),
-                    Text("Rating: ${movie.imdbRating} / 10"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text("Released ${movie.released}"),
-                    Text("${movie.runtime}"),
-                    Text("${movie.rated}"),
-
-                  ],
-                ),
-
-
-              ],
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 54.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        child: Text(
+                          "${movie.title}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Rating: ${movie.imdbRating} / 10",
+                        style: mainTextStyle(),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Released ${movie.released}",
+                        style: mainTextStyle(),
+                      ),
+                      Text(
+                        "${movie.runtime}",
+                        style: mainTextStyle(),
+                      ),
+                      Text(
+                        "${movie.rated}",
+                        style: mainTextStyle(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-
         ),
       ),
-    onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MovieListViewDetails(
-                              movieName: movie.title,
-                              movie: movie)));
-                },
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MovieListViewDetails(
+                    movieName: movie.title, movie: movie)));
+      },
     );
   }
 
-  Widget movieImage(String imageUrl){
+  TextStyle mainTextStyle() {
+    return TextStyle(fontSize: 15.0, color: Colors.grey);
+  }
+
+  Widget movieImage(String imageUrl) {
     return Container(
       width: 100,
       height: 100,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         image: DecorationImage(
-          image: NetworkImage(imageUrl ?? 'https://image.shutterstock.com/image-vector/upset-magnifying-glass-cute-not-260nw-1127749553.jpg'),
+          image: NetworkImage(imageUrl ??
+              'https://image.shutterstock.com/image-vector/upset-magnifying-glass-cute-not-260nw-1127749553.jpg'),
           fit: BoxFit.cover,
         ),
       ),
@@ -155,15 +148,92 @@ class MovieListViewDetails extends StatelessWidget {
         title: Text("Movies"),
         backgroundColor: Colors.blueGrey.shade900,
       ),
-      body: Center(
-        child: Container(
-          child: RaisedButton(
-              child: Text("Go Back ${this.movieName}"),
-              onPressed: () {
-                Navigator.pop(context);
-              }),
-        ),
+      body: ListView(
+        children: <Widget>[
+          MovieDetailsThumbnail(
+            thumbnail: movie.images[0],
+          ),
+          MovieDetailsHeaderWithPoster(movie: movie,)
+        ],
       ),
+
     );
   }
 }
+
+class MovieDetailsThumbnail extends StatelessWidget {
+  final String thumbnail;
+
+  const MovieDetailsThumbnail({Key key, this.thumbnail}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        Stack(alignment: Alignment.center, children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 190,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: NetworkImage(thumbnail),
+              fit: BoxFit.cover,
+            )),
+          ),
+          Icon(
+            Icons.play_circle_outline,
+            size: 100,
+            color: Colors.white,
+          ),
+        ]),
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0x00f5f5f5), Color(0xfff5f5f5)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          height: 70,
+        ),
+      ],
+    );
+  }
+}
+
+class MovieDetailsHeaderWithPoster extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailsHeaderWithPoster({Key key, this.movie}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    child: Row(
+      children: <Widget>[
+        MoviePoster(poster: movie.images[0].toString())
+      ],
+    ),);
+  }
+}
+
+class MoviePoster extends StatelessWidget {
+  final String poster;
+
+  const MoviePoster({Key key, this.poster}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    var borderRadius = BorderRadius.all(Radius.circular(10.0));
+    return Card(child: ClipRRect(
+      borderRadius: borderRadius,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 4,
+        height: 160,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: NetworkImage(poster),
+          fit: BoxFit.cover,)
+        ),
+      ),
+    ),);
+  }
+}
+
